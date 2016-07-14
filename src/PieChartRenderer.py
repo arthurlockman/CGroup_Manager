8216a814d813c0a153faae92464e7df976548216
Arthur Lockman <hello@rthr.me>
2016-07-14 11:22:23 -0400

    Pie chart can now render sections.

--- src/PieChartRenderer.py
+++ src/PieChartRenderer.py
@@ -8,11 +8,12 @@ class PieChartRenderer:
         self.area = drawingArea
         self.area.connect('draw', self.draw)
         self.chart_color = color
+        self.sections = [45, 10, 60, 90]
 
     def draw(self, widget, event):
         cr = widget.get_property('window').cairo_create()
 
-        cr.set_line_width(5)
+        cr.set_line_width(2)
         cr.set_source_rgb(0, 0, 0)
 
         w = widget.get_allocation().width
@@ -23,10 +24,26 @@ class PieChartRenderer:
         else:
             r = h / 2 - (h / 10.0)
         cr.translate(w/2, h/2)
-        cr.arc(0, 0, r, 0, 2*math.pi)
-        cr.stroke_preserve()
+        accum = 0;
+        for section in self.sections:
+            x = r * math.cos(math.radians(section + accum))
+            y = r * math.sin(math.radians(section + accum))
+            cr.set_source_rgb(0, 0, 0)
+            cr.arc(0, 0, r, math.radians(accum), math.radians(section + accum))
+            cr.line_to(0, 0)
+            accum += section
+            cr.stroke_preserve()
+            cr.close_path()
+            cr.set_source_rgba(self.chart_color[0], self.chart_color[1],
+                               self.chart_color[2], 0.6)
+            cr.fill()
 
-        cr.set_source_rgb(self.chart_color[0], self.chart_color[1],
-                          self.chart_color[2])
+        cr.set_source_rgb(0, 0, 0)
+        cr.arc(0, 0, r, math.radians(accum), 2*math.pi)
+        cr.line_to(0, 0)
+        cr.stroke_preserve()
+        cr.close_path()
+        cr.set_source_rgba(self.chart_color[0], self.chart_color[1],
+                           self.chart_color[2], 0.1)
         cr.fill()
 
