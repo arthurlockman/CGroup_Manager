09a0a014fd4fc95a0fffa8b88f390cdee372cf84
Arthur Lockman <hello@rthr.me>
2016-07-14 09:30:45 -0400

    Updated setup script.

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@
 virtualenv -p python3 venv
 source ./venv/bin/activate
 ln -s /usr/lib64/python3.5/site-packages/gi ./venv/lib64/python3.5/site-packages/
-source ./venv/bin/activate
+cd pycairo
+python setup.py install
+cd ..
 pip install -r requirements.txt
